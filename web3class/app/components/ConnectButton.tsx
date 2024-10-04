3698815ffbf1d843220c347c372bb2ebb79f9e3e
jhfnetboy <jhfnetboy@gmail.com>
2024-10-04 18:28:40 +0700

    connected

--- web3class/app/components/ConnectButton.tsx
+++ web3class/app/components/ConnectButton.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import { useWeb3Modal } from "@web3modal/wagmi/react";
+import { useAccount } from "wagmi"; // Import useAccount hook
 
 export default function ConnectButton() {
   const { open } = useWeb3Modal();
+  const { isConnected } = useAccount(); // Get connection status
 
   return (
     <button
       onClick={() => open()}
       className="bg-blue-500 text-white p-2 rounded mb-4"
     >
-      Connect Wallet
+          {isConnected ? "Connected" : "Connect Wallet"}
     </button>
   );
 }
